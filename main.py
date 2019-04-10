6e156ec3a8e4f40b0ae168722a5cbed834e7084d
dcrowder2 <dcrowder2@alaska.edu>
2019-04-10 13:43:09 -0800

    Teams seperate

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# import tensorflow
-# import keras
+import tensorflow
 import numpy as np
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.utils import plot_model
 import sqlite3
+import matplotlib.pyplot as plt
 
 
 def get_games(team_id):
@@ -13,75 +14,105 @@ def get_games(team_id):
 	cur.execute("Select * from games where winning_id = ? or losing_id = ?", (team_id, team_id))
 	data = cur.fetchall()
 	array = np.array(data)
-	wins = np.array([x for x in array if x[1] == team_id])
-	losses = np.array([x for x in array if x[2] == team_id])
+	# wins = np.array([x for x in array if x[1] == team_id])
+	# losses = np.array([x for x in array if x[2] == team_id])
 	database.close()
 	return array
 
 
-def get_data(team_id, games_back, include_opponent):
-	games = get_games(team_id)
+def get_data(team_id, games_back, include_opponent, games):
+
 	if include_opponent:
 		ret_array = []
 		for i in range(games_back):
 			if games[i][1] == team_id:
-				ret_array.append([1, games[i][3], games[i][4]])
+				ret_array.append([team_id, 1, games[i+1][3], games[i+1][4]])
 			else:
-				ret_array.append([0, games[i][4], games[i][3]])
+				ret_array.append([team_id, 0, games[i+1][4], games[i+1][3]])
 		return np.array(ret_array)
 	else:
 		ret_array = []
 		for i in range(games_back):
 			if games[i][1] == team_id:
-				ret_array.append([1, games[i][3]])
+				ret_array.append([team_id, 1, games[i+1][3]])
 			else:
-				ret_array.append([0, games[i][4]])
+				ret_array.append([team_id, 0, games[i+1][4]])
+
 		return np.array(ret_array)
 
 
-# def train(data, network, pixels, v):
-#
-# 	labels = pixels[:, 0].reshape(-1, 1)
-#
-# 	return network.fit(data, labels, epochs=1000, batch_size=250, verbose=v)
-#
-#
-# def test(data, network, pixels):
-#
-# 	labels = pixels[:, 0].reshape(-1, 1)
-#
-# 	score = network.evaluate(data, labels, batch_size=100)
-# 	return score
-#
-#
-# def update_model(image, train_pix, network):
-#
-# 	return train(image, network, train_pix, 0)
+def get_all_data(team_id, include_opponent, games_back):
+	ret_array = []
+	games = get_games(team_id)
+	# Getting all the games save for the last one since there is no game before the first one
+	while games.shape[0] > 1:
+		ret_array.extend(get_data(team_id, games_back, include_opponent, games))
+		games = games[games_back:]
+	return np.array(ret_array)
+
+
+def get_all_teams(games_back, include_opponent):
+	ret_array = []
+	for i in range(1, 33):
+		ret_array.append(get_all_data(i, include_opponent, games_back))
+	return np.array(ret_array)
+
+
+def split(data):
+	split_point = (data.shape[0] * 4) // 5
+	return np.array(data[:split_point]), np.array(data[split_point:])
+
+
+def train(data, network, v):
+
+	labels = data[:, 1].reshape(-1, 1)
+
+	return network.fit(data[:, 2:], labels, epochs=1000, batch_size=100, verbose=v)
+
+
+def test(data, network):
+
+	labels = data[:, 1].reshape(-1, 1)
+
+	score = network.evaluate(data[:, 2:], labels, batch_size=100)
+	return score
 
 
 if __name__ == '__main__':
-	data1 = get_data(29, 200, True)
-	data2 = get_data(29, 100, False)
-	print("yeah")
+	data_all = get_all_teams(1, False)
+	train_plots = []
+	test_plots =[]
+	for team in data_all:
+		train_data, test_data = split(team)
 
-	# model = Sequential()
-	#
-	#
-	# model.add(Dense(10, activation='tanh', input_dim=12, kernel_initializer='uniform'))
-	#
-	# model.add(Dropout(0.35))
-	#
-	#
-	# model.add(Dense(8, activation='tanh', kernel_initializer='uniform'))
-	# model.add(Dropout(0.35))
-	#
-	#
-	# model.add(Dense(1, activation='tanh', kernel_initializer='uniform'))
-	#
-	#
-	# model.compile(loss='binary_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-	#
-	# history = train(data, model, train_pixels, 1)
-	#
-	# accuracy = test(data, model, test_pixels)
-	# print(accuracy)
+		model = Sequential()
+
+		model.add(Dense(10, activation='tanh', input_dim=1, kernel_initializer='uniform'))
+		model.add(Dropout(0.35))
+
+		model.add(Dense(8, activation='tanh', kernel_initializer='uniform'))
+		model.add(Dropout(0.35))
+
+		model.add(Dense(1, activation='tanh', kernel_initializer='uniform'))
+
+		model.compile(loss='binary_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+		history = train(train_data, model, 1)
+
+		accuracy = test(test_data, model)
+
+		# plot_model(model)
+		temp = [float(team[0][0])]
+		temp.extend(history.history["acc"])
+		train_plots.append(temp)
+		test_plots.append(accuracy[1])
+		print(accuracy[1])
+	train_plots = np.array(train_plots)
+	for plot in train_plots:
+		plt.plot(plot[1:])
+	plt.legend(train_plots[:, 0], loc='upper left')
+	plt.title('Model accuracy')
+	plt.ylabel('Accuracy')
+	plt.xlabel('Epoch')
+
+	plt.show()
